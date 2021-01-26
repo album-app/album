cbf27ef729d67c9334c054cb5854c395131bb59e
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2021-01-26 19:24:25 +0100

    WIP: passed arguments to hips call
    WIP: try to run the cellpose model directly

--- examples/cellpose_prediction.py
+++ examples/cellpose_prediction.py
@@ -1,8 +1,9 @@
 # Copyright 2021 Kyle Harrington
 
-import hips
 import subprocess
+import os
 import sys
+import hips
 
 global args
 
@@ -12,7 +13,13 @@ def hips_init():
     Initialization to setup this HIPS
     """
     global args
-    args = {}
+
+    # name = str(hips.get_active_hips()['name']),
+    #
+    # sys.path.insert(0, get_deployment_path())
+    # exec("import {} as hips_module".format(name[0]))
+    # argument_parsing()
+
     # trying to create environment via cli - cannot switch environement within this process
     # also Command "conda env create -f yamlfile.yaml" is not possible via cli
     # one could run the cellpose_prediction() with the python env created here ???...
@@ -21,24 +28,71 @@ def hips_init():
     # conda.run_command(conda.Commands.CREATE, ["-n", "cellpose"])
 
 
+def get_deployment_path():
+    return "/home/jpa/PycharmProjects/cellpose-git/"
+
+
 def cellpose_prediction():
     """
     This is the main entry point of this HIPS
     """
     global args
+
+    import importlib
+    import importlib.abc
     import runpy
+    #
+    #name = str(hips.get_active_hips()['name']),
+    #name = name[0]
+    sys.path.insert(0, os.path.join(get_deployment_path()))
+    #
+    #
+    # #runpy.run_module(name)
+    #
+    #
+    #importlib.import_module(name)
+    #
+    # y = importlib.abc.Loader()
+    # y.exec_module()
+    #
+    # print(type(x))
+#     # #sys.modules[name].__main__()
+#     # print(help(sys.modules[name]))
+#     environment_system = {**os.environ.copy(), **{'PYTHONPATH': get_deployment_path()}}
+#
+# # /home/jpa/PycharmProjects/cellpose/cellpose/t.py
+#     subprocess.run([
+#         'python', '/home/jpa/t/t.py', '--dir', '/home/jpa/tmp/', '--pretrained_model', 'cyto', '--save_png'
+#     ], env=environment_system)
+#
+#     os.chdir(get_deployment_path())
+#     subprocess.run([
+#         'python', '-m', 'cellpose', '--dir', '/home/jpa/tmp/', '--pretrained_model', 'cyto', '--save_png'
+#     ])
+    os.chdir(get_deployment_path())
+    print(os.getcwd())
+    runpy.run_module('cellpose')
 
-    # hardcoded path to cellpose repo - needs to be downloaded in init process?
-    sys.path.insert(0, '/home/jpa/PycharmProjects/cellpose/')
-
-    # run the cellpose module
-    # gennot get the arguments to work here - will switch to subprocess now
-    #runpy.run_module('cellpose')
+    #exec("cellpose.main()")
+    #
+    # environment_name = hips.get_environment_name(hips.get_active_hips())
+    # environment = os.path.join(os.path.expanduser('~/'), 'anaconda3',
+    #                            'envs', environment_name)
+    #
+    # subprocess_args = [
+    #     'conda', 'run', '--no-capture-output', '--prefix', environment,
+    #     'python', '-m', name[0],
+    # ]
+    #
+    # environment_system = {**os.environ.copy(), **{'PYTHONPATH': get_deployment_path()}}
+    # name = str(hips.get_active_hips()['name']),
+    #
+    # sys.path.insert(0, get_deployment_path())
+    # exec("import {} as hips_module".format(name[0]))
+    # exec("hips_module.__main__.py")
 
     # not the best solution to run a module...
-    subprocess.run([
-        'python', '-m', 'cellpose.cellpose', '--dir', '/home/jpa/tmp/', '--pretrained_model', 'cyto', '--save_png'
-    ])
+    #subprocess.run(subprocess_args, env=environment_system)
 
 
 hips.setup(
---
+++ utils/copy_argparse.py
@@ -0,0 +1,90 @@
+import re
+import autopep8
+
+
+class ExtractArgparseError(Exception):
+    """Exception class for argument extraction"""
+
+    def __init__(self, short_message, long_message):
+        self.short_message = short_message
+        self.long_message = long_message
+
+
+def get_line_indent(line):
+    return len(re.findall('^[ ]*', line)[0])/4
+
+
+def auto_format(file):
+    fixed_file = autopep8.fix_file(file)
+    return fixed_file
+
+
+def is_comment(line):
+    if re.match('^[ ]*[#]', line):
+        return True
+    return False
+
+
+def is_blank_line(line):
+    if re.match('^\n', line):
+        return True
+    return False
+
+
+def to_executable(argument_parsing):
+    executable = ""
+    for arg in argument_parsing:
+        executable += arg
+    return executable
+
+
+def extract_argparse(file):
+    """
+    Extracts the argument parsing out of a file.
+    Returns all add_argument() calls
+    """
+    argument_parsing = ["global args\n"]
+
+    # one could think of autoformat stuff to be sure indents are correct - because the following should NOT happen
+    # arser.add_argument(\'--mkldnn\', action=\'store_true\', help=\'for
+    # mxnet, force MXNET_SUBGRAPH_BACKEND = "MKLDNN"\') <-- wrong indent, but allowed by python parser. Would be fixed
+    # with autoformat.
+    # string = auto_format(file)
+
+    with open(file, 'r') as f:
+        # search for argparse line
+        while True:
+            line = f.readline()
+            if not line:
+                break
+
+            # store line indent to begin with
+            previous_indent = get_line_indent(line)
+
+            # all lines which follow should belong to argument parsing
+            if "argparse.ArgumentParser" in line:
+                argument_parsing.append(line.strip() + '\n')
+                while True:
+                    line = f.readline()
+                    if not line:
+                        break
+                    if ".add_argument(" in line:  # adding argument
+                        argument_parsing.append(line.strip() + '\n')
+                    elif ".parse_args(" in line:  # end of collecting arguments
+                        argument_parsing.append("args = " + line.strip() + '\n')
+                        return to_executable(argument_parsing)
+                    elif previous_indent < get_line_indent(line):  # linebreak within add_argument command
+                        argument_parsing[-1] = argument_parsing[-1][:-1] + line.strip() + '\n'
+                        continue
+                    elif is_comment(line) or is_blank_line(line):  # blank lines or comments are skipped
+                        continue
+                    else:  # conditional argument parsing or some logic between two add_argument commands is prohibited
+                        raise ExtractArgparseError("Argument parsing went wrong! Illegal sequence of commands. ",
+                                                   "Argument parsing went wrong! "
+                                                   "Detected line '{}' after ArgumentParser initiation.".format(line))
+                    # set new indent
+                    previous_indent = get_line_indent(line)
+    return ""
+
+
+#print(extract_argparse("/home/jpa/PycharmProjects/cellpose/cellpose/__main__.py"))
