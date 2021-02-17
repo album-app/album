205d0e465ffc18e9173b6a4e64f3457198f7d406
jpalbrecht <5586891-jpalbrecht91@users.noreply.gitlab.com>
2021-02-17 13:24:59 +0000

    Restructure hips.

--- hips/cmdline.py
+++ hips/cmdline.py
@@ -3,26 +3,74 @@ import argparse
 from hips.deploy import deploy
 from hips.run import run
 from hips.repl import repl
+from hips.search import search
+from hips.install import install
+from hips.remove import remove
+from hips.containerize import containerize
+from hips.tutorial import tutorial
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process a HIP Solution')
+    parser = argparse.ArgumentParser(
+        description=
+        'Helmholtz Imaging Platform (HIP) Solutions framework for running, building, and deploying generalized imaging solutions'
+    )
     subparsers = parser.add_subparsers(help='sub-command help')
 
     # run
-    parser_run = subparsers.add_parser('run', help='run help')
+    parser_run = subparsers.add_parser('run', help='run a HIP Solution')
     parser_run.add_argument('path', type=str, help='path for the HIPS file')
     parser_run.set_defaults(func=run)
 
     # repl
-    parser_repl = subparsers.add_parser('repl', help='repl help')
+    parser_repl = subparsers.add_parser(
+        'repl', help='get an interactive repl for a HIP Solution')
     parser_repl.add_argument('path', type=str, help='path for the HIPS file')
     parser_repl.set_defaults(func=repl)
 
     # deploy
-    parser_run = subparsers.add_parser('deploy', help='deploy help')
-    parser_run.add_argument('path', type=str, help='path for the HIPS file')
-    parser_run.set_defaults(func=deploy)
+    parser_deploy = subparsers.add_parser('deploy',
+                                          help='deploy a HIP Solution')
+    parser_deploy.add_argument('path', type=str, help='path for the HIPS file')
+    parser_deploy.set_defaults(func=deploy)
+
+    # search
+    parser_search = subparsers.add_parser(
+        'search', help='search for a HIP Solution using keywords')
+    parser_search.set_defaults(func=search)
+
+    # install
+    parser_install = subparsers.add_parser('install',
+                                           help='install a HIP Solution')
+    parser_install.add_argument('path',
+                                type=str,
+                                help='path for the HIPS file')
+    parser_install.set_defaults(func=install)
+
+    # remove
+    parser_remove = subparsers.add_parser('remove',
+                                          help='remove a HIP Solution')
+    parser_remove.add_argument('path', type=str, help='path for the HIPS file')
+    parser_remove.set_defaults(func=remove)
+
+    # containerize
+    parser_containerize = subparsers.add_parser(
+        'containerize',
+        help='create a Singularity container for a HIP Solution')
+    parser_containerize.add_argument('path',
+                                     type=str,
+                                     help='path for the HIPS file')
+    parser_containerize.set_defaults(func=containerize)
+
+    # tutorial
+    parser_tutorial = subparsers.add_parser(
+        'tutorial', help='run a tutorial for a HIP Solution')
+    parser_tutorial.add_argument('path',
+                                 type=str,
+                                 help='path for the HIPS file')
+    parser_tutorial.set_defaults(func=tutorial)
+
+    # ToDo: clean all hips environments
 
     # Parse args at the "hips" level
     args = parser.parse_args(sys.argv[1:3])
---
+++ hips/search.py
@@ -0,0 +1,67 @@
+import yaml
+import argparse
+from hips import Hips, get_active_hips
+import sys
+from xdg import xdg_cache_home
+import os
+from acora import AcoraBuilder
+from functools import reduce
+import operator
+import gitdir
+
+
+def hips_cache_home():
+    """
+    Return the path for the HIPS cache, 
+    e.g. ~/.config/hips/cache
+    """
+    return os.path.join(xdg_cache_home(), 'hips', 'cache')
+
+
+def load_search_index():
+    """
+    Load a package search index
+    This is a dictionary of package name:dicts
+    """
+    search_cache = os.path.join(hips_cache_home(), 'catalog')
+    gitdir.download(
+        'https://gitlab.com/ida-mdc/hips-catalog/-/tree/main/catalog',
+        output_dir=search_cache)
+
+    search_index = {}
+    listing = os.listdir(search_cache)
+    for filename in listing:
+        yml = yaml.load(os.path.join(search_cache, filename))
+        search_index[yml['name']] = yml
+
+    return search_index
+
+
+def search(args):
+    """
+    Function corresponding to the `search` subcommand of `hips`
+    Searches through hips catalog to find closest matching hips
+    """
+    sys.argv = ['search'] + sys.argv
+    parser = argparse.ArgumentParser(description='HIPS search')
+    parser.add_argument('keywords',
+                        type=str,
+                        nargs='+',
+                        help='Search keywords')
+    search_args = parser.parse_args()
+
+    builder = AcoraBuilder()
+    builder.update(search_args.keywords)
+    searcher = builder.build()
+
+    search_index = load_search_index()
+    match_score = {}
+    for package_name, package in search_index.items():
+        package_result = searcher.findall(package_name +
+                                          package['description'])
+        score = reduce(lambda a, b: a + b, [v[1] for v in package_result])
+        match_score[package['name']] = score
+
+    sorted_results = sorted(match_score.items(), key=operator.itemgetter(1))
+    print('Search results for "%s"' % search_args.keywords)
+    print(sorted_results)
