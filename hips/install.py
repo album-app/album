f2919ba172484401c398665551e2dafec116fd1f
frauzufall <mail@frauzufall.de>
2021-03-04 17:59:29 +0100

    hips/install: Add possibility to define HIPS as dependencies #18
    HIPS defined as dependencies of a solution will be installed when
    installing the solution.
    
    Example usage in a solution hips.setup:
    ```
    dependencies={
        'environment_name': 'hips_full',
        'hips': [{
            "group": "ida-mdc",
            "name": "blender",
            "version": "0.1.0"
        }]
    })
    ```

--- hips/install.py
+++ hips/install.py
@@ -1,4 +1,9 @@
 from hips import Hips, get_active_hips
+from utils import subcommand
+import logging
+
+
+module_logger = logging.getLogger('hips')
 
 
 def install(args):
@@ -7,5 +12,24 @@ def install(args):
     hips_script = open(args.path).read()
     exec(hips_script)
     hips = get_active_hips()
+    if 'dependencies' in dir(hips):
+        dependencies = hips['dependencies']
+        if 'hips' in dependencies:
+            install_hips_dependencies(dependencies['hips'])
 
-    print('This would install: %s' % hips['name'])
+    module_logger.info('Installed %s' % hips['name'])
+
+
+def install_hips_dependencies(args):
+    for hips in args:
+        hips_script = resolve_hips(hips)
+        subcommand.run("python -m hips install " + hips_script)
+
+
+def resolve_hips(hips):
+    # TODO properly implement this - i.e. match with zenodo
+    path = ""
+    if hips["group"] == "ida-mdc" and hips["name"] == "blender":
+        path = "./examples/blender.py"
+    return path
+
