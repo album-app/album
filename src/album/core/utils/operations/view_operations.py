d664cd93cd7c7acaacf3724199e951f0ea719bd3
frauzufall <mail@frauzufall.de>
2022-01-18 17:53:35 +0100

    view_operations: display install status

--- src/album/core/utils/operations/view_operations.py
+++ src/album/core/utils/operations/view_operations.py
@@ -95,14 +95,17 @@ def get_index_as_string(index_dict: dict):
             res += '├─ catalog_id: %s\n' % catalog['catalog_id']
             if len(catalog['solutions']) > 0:
                 res += '├─ deletable: %s\n' % catalog['deletable']
-                res += '└─ solutions:\n'
+                res += '└─ [installed] solutions:\n'
                 for i, solution in enumerate(catalog['solutions']):
+                    installed = ' '
+                    if solution['internal']['installed']:
+                        installed = 'x'
                     if i is len(catalog['solutions']) - 1:
-                        res += '   └─ %s:%s:%s\n' % (
-                            solution['setup']['group'], solution['setup']['name'], solution['setup']['version'])
+                        res += '   └─ [%s] %s:%s:%s\n' % (
+                            installed, solution['setup']['group'], solution['setup']['name'], solution['setup']['version'])
                     else:
-                        res += '   ├─ %s:%s:%s\n' % (
-                            solution['setup']['group'], solution['setup']['name'], solution['setup']['version'])
+                        res += '   ├─ [%s] %s:%s:%s\n' % (
+                            installed, solution['setup']['group'], solution['setup']['name'], solution['setup']['version'])
             else:
                 res += '└─ deletable: %s\n' % catalog['deletable']
     return res
