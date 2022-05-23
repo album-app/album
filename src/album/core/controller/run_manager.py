772e3fb9c13e863c5d9b8d80d7c1149d6b5ba114
Deborah Schmidt <mail@frauzufall.de>
2022-05-23 11:04:59 +0200

    run_manager: add events, load plugins

--- src/album/core/controller/run_manager.py
+++ src/album/core/controller/run_manager.py
@@ -1,10 +1,15 @@
 from queue import Queue
 from typing import List
 
+import pkg_resources
+
+from album.runner import album_logging
+
 from album.core.api.controller.controller import IAlbumController
 from album.core.api.controller.run_manager import IRunManager
 from album.core.api.model.collection_solution import ICollectionSolution
-from album.runner import album_logging
+from album.core.model.default_values import DefaultValues
+from album.core.model.event import Event
 from album.runner.core.api.model.solution import ISolution
 from album.runner.core.model.script_creator import ScriptCreatorRun
 
@@ -49,12 +54,15 @@ class RunManager(IRunManager):
     def run(self, solution_to_resolve: str, run_immediately=False, argv=None):
         """Run an already loaded solution."""
         resolve_result = self.album.collection_manager().resolve_installed_and_load(solution_to_resolve)
+        coordinates = resolve_result.loaded_solution().coordinates()
         if not resolve_result.catalog:
-            module_logger().debug('solution loaded locally: %s...' % str(resolve_result.loaded_solution().coordinates()))
+            module_logger().debug('solution loaded locally: %s...' % str(coordinates))
         else:
-            module_logger().debug('solution loaded from catalog: \"%s\"...' % str(resolve_result.loaded_solution().coordinates()))
+            module_logger().debug('solution loaded from catalog: \"%s\"...' % str(coordinates))
         module_logger().debug(
-            "Initializing script to run \"%s\"" % resolve_result.loaded_solution().coordinates())
+            "Initializing script to run \"%s\"" % coordinates)
+
+        self.load_plugins(resolve_result)
 
         if argv is None:
             argv = [""]
@@ -65,8 +73,38 @@ class RunManager(IRunManager):
         # builds the queue
         self.album.script_manager().build_queue(resolve_result, que, ScriptCreatorRun(), run_immediately, argv)
 
+        self.album.event_manager().publish(Event(DefaultValues.before_run_event_name.value), resolve_result.coordinates())
+
         # runs the queue
-        self.album.script_manager().run_queue(que)
+        try:
+            self.album.script_manager().run_queue(que)
+        finally:
+            self.album.event_manager().publish(Event(DefaultValues.after_run_event_name.value), resolve_result.coordinates())
 
         module_logger().debug(
-            "Finished running script for solution \"%s\"" % resolve_result.loaded_solution().coordinates().name())
+            "Finished running script for solution \"%s\"" % coordinates.name())
+
+    def load_plugins(self, resolve_result):
+        # process solution plugins
+        module_logger().debug('Processing plugins...')
+        if 'dependencies' in resolve_result.loaded_solution().setup():
+            deps = resolve_result.loaded_solution().setup()['dependencies']
+            if 'plugins' in deps:
+                module_logger().debug('Processing solution plugins: %s' % deps['plugins'])
+                available_plugins = pkg_resources.iter_entry_points('plugins_album')
+                for plugin in deps['plugins']:
+                    found = False
+                    for entry_point in available_plugins:
+                        if entry_point.name == plugin['id']:
+                            module_logger().debug('Found plugin %s, initializing activation..' % plugin['id'])
+                            plugin_call = entry_point.load()
+                            plugin_args = plugin['args'] if 'args' in plugin else {}
+                            from album.api import Album
+                            plugin_call(Album(self.album), resolve_result.coordinates(), plugin_args)
+                            module_logger().debug('Plugin %s activated.' % plugin['id'])
+                            found = True
+                            break
+                    if not found:
+                        raise LookupError('Cannot find plugin %s - please make sure the package providing this plugin '
+                                          'is installed into the album environment.' % plugin['id'])
+        module_logger().debug('Plugins processed.')
