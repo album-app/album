7f84402139bf3b509571e8692e4491e0dfba8830
Lucas Rieckert <lucas.rieckert@outlook.de>
2023-01-23 16:31:50 +0100

    Tests for ssh_check and faulty catalog cloning

--- test/unit/core/controller/test_clone_manager.py
+++ test/unit/core/controller/test_clone_manager.py
@@ -2,6 +2,13 @@ import unittest
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
+from git import GitCommandError
+
+import album
+from album.core.utils.operations.git_operations import (
+    create_bare_repository,
+    clone_repository)
+from album.core.utils.operations.file_operations import create_path_recursively
 from album.core.model.resolve_result import ResolveResult
 from test.unit.test_unit_core_common import TestUnitCoreCommon
 
@@ -11,6 +18,7 @@ class TestCondaManager(TestUnitCoreCommon):
         super().setUp()
         self.setup_solution_no_env()
         self.clone_manager = self.album_controller.clone_manager()
+        self.catalog = self.setup_empty_catalog("test_catalog")
 
     def tearDown(self) -> None:
         super().tearDown()
@@ -48,3 +56,79 @@ class TestCondaManager(TestUnitCoreCommon):
     def test__try_cloning_catalog_template(self):
         # todo: implement
         pass
+
+    @patch("album.core.controller.clone_manager.clone_repository")
+    def test_setup_repository_from_template_with_valid_url(self, git_clone_mock):
+        # mocks
+        _copy_template_into_repository = MagicMock()
+        self.clone_manager._copy_template_into_repository = _copy_template_into_repository
+
+        # prepare
+        git_https_url = "https://gitlab.com/album-app/album.git"
+
+        # call
+        self.clone_manager.setup_repository_from_template(git_https_url, self.tmp_dir.name, "test_catalog")
+
+        # assert
+        _copy_template_into_repository.assert_called_once()
+
+    @patch("album.core.controller.clone_manager.clone_repository")
+    def test_setup_repository_from_template_with_valid_ssh(self, git_clone_mock):
+        # mocks
+        _copy_template_into_repository = MagicMock()
+        self.clone_manager._copy_template_into_repository = _copy_template_into_repository
+
+        # prepare
+        git_https_url = "git@gitlab.com:album-app/album.git"
+
+        # call
+        #with patch("album.core.utils.operations.git_operations.clone_repository") as git_clone_mock:
+        self.clone_manager.setup_repository_from_template(git_https_url, self.tmp_dir.name, "test_catalog")
+
+        # assert
+        _copy_template_into_repository.assert_called_once()
+
+    @patch("album.core.controller.clone_manager.clone_repository")
+    def test_setup_repository_from_template_with_valid_ssh_2(self, git_clone_mock):
+        # mocks
+        _copy_template_into_repository = MagicMock()
+        self.clone_manager._copy_template_into_repository = _copy_template_into_repository
+
+        # prepare
+        git_https_url = "ssh://git@gitlab.com:album-app/album.git"
+
+        # call
+        self.clone_manager.setup_repository_from_template(git_https_url, self.tmp_dir.name, "test_catalog")
+
+        # assert
+        _copy_template_into_repository.assert_called_once()
+
+    def test_setup_repository_from_template_with_invalid_ssh(self):
+        # prepare
+        git_https_url = "git@valid_ssh:format.git"
+
+        # call and assert
+        with self.assertRaises(GitCommandError):
+            self.clone_manager.setup_repository_from_template(git_https_url, self.tmp_dir.name, "test_catalog")
+
+    @patch("album.core.controller.clone_manager.clone_repository")
+    def test_setup_repository_from_template_with_local_path(self, git_clone_mock):
+        # mocks
+        _copy_template_into_repository = MagicMock()
+        self.clone_manager._copy_template_into_repository = _copy_template_into_repository
+
+        create_bare_repository = MagicMock()
+        album.core.controller.clone_manager.create_bare_repository = create_bare_repository
+
+        create_path_recursively = MagicMock()
+        album.core.controller.clone_manager.create_path_recursively = create_path_recursively
+
+        # call
+        self.clone_manager.setup_repository_from_template(Path(self.tmp_dir.name), self.tmp_dir.name, Path(self.tmp_dir.name).joinpath("test"), "test_catalog")
+
+        # assert
+        _copy_template_into_repository.assert_called_once()
+        create_bare_repository.assert_called_once()
+        create_path_recursively.assert_called_once()
+
+
